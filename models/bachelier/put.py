51d912b927267f52ab99725edeb44483f7e191ce
Jakob Petersen <petersen.jakob@icloud.com>
2021-11-06 10:01:56 +0100

    For Bachelier model, analytical formulas for greeks of European put option revised.

--- models/bachelier/put.py
+++ models/bachelier/put.py
@@ -1,68 +1,58 @@
 import math
+import numpy as np
 from scipy.stats import norm
 
 import models.bachelier.option as option
+import utils.global_types as global_types
 import utils.payoffs as payoffs
 
 
-class Put(option.Option):
-    """
-    European put option in Bachelier model
-    """
+class Put(option.VanillaOption):
+    """European put option in Bachelier model."""
+
     def __init__(self, vol, strike, expiry):
         super().__init__(vol, strike, expiry)
+        self._option_type = global_types.OptionType.EUROPEAN_PUT
 
-    def payoff(self, spot):
-        """
-        Payoff of European put option
+    @property
+    def option_type(self):
+        return self._option_type
 
-        Parameters
-        ----------
-        spot : float / numpy.ndarray
-
-        Returns
-        -------
-        float / numpy.ndarray
-        """
+    def payoff(self, spot: (float, np.ndarray)) -> (float, np.ndarray):
         return payoffs.put(spot, self.strike)
 
-    def price(self, spot, time):
-        """
-        Price of European put option in Bachelier model
-
-        Parameters
-        ----------
-        spot : float / numpy.ndarray
-        time : float
-
-        Returns
-        -------
-        float / numpy.ndarray
-        """
+    def price(self,
+              spot: (float, np.ndarray),
+              time: float) -> (float, np.ndarray):
         factor1 = self.strike - spot
         factor2 = self.vol * math.sqrt(self.expiry - time)
         return factor1 * norm.cdf(factor1 / factor2) \
             + factor2 * norm.pdf(factor1 / factor2)
 
-    def delta(self, spot, time):
-        """
-        Delta of European put option in Bachelier model
-
-        Parameters
-        ----------
-        spot : float / numpy.ndarray
-        time : float
-
-        Returns
-        -------
-        float / numpy.ndarray
-        """
+    def delta(self,
+              spot: (float, np.ndarray),
+              time: float) -> (float, np.ndarray):
         factor1 = self.strike - spot
         factor2 = self.vol * math.sqrt(self.expiry - time)
-        return norm.cdf(factor1 / factor2)
+        return -norm.cdf(factor1 / factor2)
 
-    def gamma(self, spot, time):
-        pass
+    def gamma(self,
+              spot: (float, np.ndarray),
+              time: float) -> (float, np.ndarray):
+        factor1 = self.strike - spot
+        factor2 = self.vol * math.sqrt(self.expiry - time)
+        return norm.pdf(factor1 / factor2) / factor2
 
-    def vega(self, spot, time):
-        pass
+    def theta(self,
+              spot: (float, np.ndarray),
+              time: float) -> (float, np.ndarray):
+        factor1 = self.strike - spot
+        factor2 = self.vol * math.sqrt(self.expiry - time)
+        return - norm.pdf(factor1 / factor2) / (2 * factor2)
+
+    def vega(self,
+             spot: (float, np.ndarray),
+             time: float) -> (float, np.ndarray):
+        factor1 = self.strike - spot
+        factor2 = self.vol * math.sqrt(self.expiry - time)
+        return math.sqrt(self.expiry - time) * norm.pdf(factor1 / factor2)
