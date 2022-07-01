3426909fcdb14b07534866f1729caf186b27c618
Jakob Petersen <petersen.jakob@icloud.com>
2022-07-01 23:37:10 +0200

    Bachelier model: mean, variance, and increment methods added to SDE class.

--- models/bachelier/sde.py
+++ models/bachelier/sde.py
@@ -5,25 +5,36 @@ from typing import Tuple
 
 import models.sde as sde
 import utils.global_types as global_types
+import utils.misc as misc
 
 
 class SDE(sde.SDE):
-    """SDE for Bachelier model
-        dS_t / S_t = vol * dW_t
+    """SDE for the Bachelier model
+        dS_t = (rate - dividend) * dt + vol * dW_t
+
+    - rate: Risk-free interest rate
+    - vol: Volatility
+    - event_grid: Event dates, i.e., trade date, payment dates, etc.
+    - dividend: Dividend yield
     """
 
-    def __init__(self, rate, vol):
+    def __init__(self,
+                 rate: float,
+                 vol: float,
+                 event_grid: np.ndarray,
+                 dividend: float = 0):
         self._rate = rate
         self._vol = vol
+        self._event_grid = event_grid
+        self._dividend = dividend
 
         self._model_name = global_types.ModelName.BACHELIER
 
+        self._price_mean = np.zeros(self._event_grid.size)
+        self._price_variance = np.zeros(self._event_grid.size)
+
     def __repr__(self):
         return f"{self._model_name} SDE object"
-
-    @property
-    def model_name(self):
-        return self._model_name
 
     @property
     def rate(self):
@@ -41,6 +52,46 @@ class SDE(sde.SDE):
     def vol(self, vol_):
         self._vol = vol_
 
+    @property
+    def event_grid(self):
+        return self._event_grid
+
+    @event_grid.setter
+    def event_grid(self,
+                   event_grid_: np.ndarray):
+        self._event_grid = event_grid_
+
+    @property
+    def dividend(self) -> float:
+        return self._dividend
+
+    @dividend.setter
+    def dividend(self,
+                 dividend_: float):
+        self._dividend = dividend_
+
+    @property
+    def model_name(self) -> str:
+        return self._model_name
+
+    def price_mean(self):
+        """Conditional mean of stock price process."""
+        self._price_mean[1:] = \
+            (self._rate - self._dividend) * np.diff(self._event_grid)
+
+    def price_variance(self):
+        """Conditional variance of stock price process."""
+        self._price_variance[1:] = self._vol ** 2 * np.diff(self._event_grid)
+
+    def price_increment(self,
+                        time_idx: int,
+                        normal_rand: (float, np.ndarray)) \
+            -> (float, np.ndarray):
+        """Increment stock price process."""
+        mean = self._price_mean[time_idx]
+        variance = self._price_variance[time_idx]
+        return mean + math.sqrt(variance) * normal_rand
+
     def path(self,
              spot: (float, np.ndarray),
              time: float,
@@ -52,16 +103,7 @@ class SDE(sde.SDE):
         antithetic : Antithetic sampling for Monte-Carlo variance
         reduction. Defaults to False.
         """
-        if antithetic:
-            if n_paths % 2 == 1:
-                raise ValueError("In antithetic sampling, "
-                                 "n_paths should be even.")
-            realizations = norm.rvs(size=n_paths // 2)
-            realizations = np.append(realizations, -realizations)
-        else:
-            realizations = norm.rvs(size=n_paths)
-        return spot * np.exp((self.rate - self.vol ** 2 / 2) * time
-                             + self.vol * math.sqrt(time) * realizations)
+        pass
 
     def path_grid(self,
                   spot: float,
@@ -69,65 +111,23 @@ class SDE(sde.SDE):
         """Generate one path, represented on time_grid, of geometric
         Brownian motion using analytic expression.
         """
-        dt = time_grid[1:] - time_grid[:-1]
-        spot_moved = spot * np.cumprod(
-            np.exp((self.rate - self.vol ** 2 / 2) * dt
-                   + self.vol * np.sqrt(dt) * norm.rvs(size=dt.shape[0])))
-        return np.append(spot, spot_moved)
+        pass
 
-    def path_wise(self,
-                  spot: np.ndarray,
-                  time: float,
-                  n_paths: int,
-                  greek: str = 'delta',
-                  antithetic: bool = False) -> Tuple[np.ndarray, np.ndarray]:
-        """Generate paths, at t = time, of geometric Brownian motion
-        using analytic expression. The paths are used for "path-wise"
-        Monte-Carlo calculation of a 'greek'.
-        todo: See 'Estimating the greeks' lecture notes by Martin Haugh (2017)
+    def paths(self,
+              spot: float,
+              n_paths: int,
+              seed: int = None,
+              antithetic: bool = False) -> np.ndarray:
+        """Generate paths represented on _event_grid of equity price
+        process using exact discretization.
 
         antithetic : Antithetic sampling for Monte-Carlo variance
         reduction. Defaults to False.
         """
-        paths = self.path(spot, time, n_paths, antithetic)
-        if greek == 'delta':
-            return paths, paths / spot
-        elif greek == 'vega':
-            wiener = (np.log(paths / spot)
-                      - (self.rate - 0.5 * self.vol ** 2) * time) / self.vol
-            return paths, paths * (wiener - self.vol * time)
-        else:
-            raise ValueError("greek can be 'delta' or 'vega'")
-
-    def likelihood_ratio(self,
-                         spot: np.ndarray,
-                         time: float,
-                         n_paths: int,
-                         greek: str = 'delta',
-                         antithetic: bool = False) \
-            -> Tuple[np.ndarray, np.ndarray]:
-        """Generate paths, at t = time, of geometric Brownian motion
-        using analytic expression. The paths are used for
-        'likelihood-ratio' Monte-Carlo calculation of a 'greek'.
-
-        The density transformation theorem is used in the derivation of
-        the expressions...
-        todo: See 'Estimating the greeks' lecture notes by Martin Haugh (2017)
-
-        antithetic : Antithetic sampling for Monte-Carlo variance
-        reduction. Defaults to False.
-        """
-        paths = self.path(spot, time, n_paths, antithetic)
-        if greek == 'delta':
-            wiener = (np.log(paths / spot)
-                      - (self.rate - 0.5 * self.vol ** 2) * time) / self.vol
-            # todo: should wiener be divided by (self.expiry - time)?
-            return paths, wiener / (spot * self.vol)
-        elif greek == 'vega':
-            normal = (np.log(paths / spot)
-                      - (self.rate - 0.5 * self.vol ** 2) * time) \
-                     / (self.vol * math.sqrt(time))
-            return paths, normal ** 2 / self.vol \
-                - normal * math.sqrt(time) - 1 / self.vol
-        else:
-            raise ValueError("greek can be 'delta' or 'vega'")
+        price = np.zeros((self._event_grid.size, n_paths))
+        price[0] = spot
+        for time_idx in range(1, self._event_grid.size):
+            realizations = misc.normal_realizations(n_paths, seed, antithetic)
+            price[time_idx] = price[time_idx - 1] \
+                + self.price_increment(time_idx, realizations)
+        return price
