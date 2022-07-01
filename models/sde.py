27aba5767dabceb986bb4f91dd6237c1c351832f
Jakob Petersen <petersen.jakob@icloud.com>
2022-07-01 23:40:55 +0200

    path and path_time_grid methods removed from SDE abstract base class.

--- models/sde.py
+++ models/sde.py
@@ -11,25 +11,7 @@ class SDE(metaclass=abc.ABCMeta):
     def model_name(self) -> str:
         pass
 
-    # TODO: Remove method
     @abc.abstractmethod
-    def path(self,
-             spot: (float, np.ndarray),
-             time: float,
-             n_paths: int,
-             antithetic: bool = False) -> (float, np.ndarray):
-        """Generate path(s) at t = time."""
-        pass
-
-    # TODO: Remove method
-    @abc.abstractmethod
-    def path_time_grid(self,
-                       spot: (float, np.ndarray),
-                       time_grid: np.ndarray) -> np.ndarray:
-        """Generate one path represented on time_grid."""
-        pass
-
-#    @abc.abstractmethod
     def paths(self,
               spot: float,
               n_paths: int,
