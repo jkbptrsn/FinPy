f5041a6f01d70ce989ca26286cdbdf64bea1d052
Jakob Petersen <petersen.jakob@icloud.com>
2022-08-19 18:34:36 +0200

    Hull-White model: Zero-coupon bond class revised.

--- models/hull_white/zero_coupon_bond.py
+++ models/hull_white/zero_coupon_bond.py
@@ -1,14 +1,27 @@
 import numpy as np
 
-import models.bonds as bonds
-import models.hull_white.sde as sde
-import utils.global_types as global_types
-import utils.misc as misc
-import utils.payoffs as payoffs
+from models import bonds
+from models.hull_white import sde
+from utils import global_types
+from utils import misc
+from utils import payoffs
 
 
 class ZCBond(sde.SDE, bonds.Bond):
-    """Zero-coupon bond class in Hull-White model."""
+    """Zero-coupon bond class for the 1-factor Hull-White model.
+
+    Attributes:
+        kappa: Speed of mean reversion.
+        vol: Volatility.
+
+        discount_curve: Discount curve represented on event-grid.
+        event_grid: Events, e.g. payment dates, represented as year
+            fractions from the as-of date.
+
+        maturity_idx: Event-grid index corresponding to maturity.
+        int_step_size: Integration/propagation step size represented as
+            a year fraction. Default is 1 / 365.
+    """
 
     def __init__(self,
                  kappa: misc.DiscreteFunc,
@@ -21,13 +34,6 @@ class ZCBond(sde.SDE, bonds.Bond):
         self.discount_curve = discount_curve
         self.maturity_idx = maturity_idx
 
-        # Run relevant setup methods of parent SDE object
-        self.setup_int_grid()
-        self.setup_kappa_vol_y()
-        self.kappa_int_grid = None
-        self.vol_int_grid = None
-        self.y_int_grid = None
-
         self.bond_type = global_types.InstrumentType.ZERO_COUPON_BOND
 
     @property
@@ -36,14 +42,14 @@ class ZCBond(sde.SDE, bonds.Bond):
 
     def payoff(self,
                spot: (float, np.ndarray)) -> (float, np.ndarray):
-        """Payoff of zero coupon bond."""
         return payoffs.zero_coupon_bond(spot)
 
     def price(self,
               spot: (float, np.ndarray),
               event_idx: int) -> (float, np.ndarray):
-        """Price of zero coupon bond."""
-        return self.calc_price(spot, event_idx, self.maturity_idx)
+        return self._calc_price(spot, event_idx, self.maturity_idx)
+
+###############################################################################
 
     def price_vector(self,
                      spot: (float, np.ndarray),
@@ -55,35 +61,47 @@ class ZCBond(sde.SDE, bonds.Bond):
         else:
             zcbond_prices = np.zeros((maturity_indices.size, 1))
         for idx, maturity_idx in enumerate(maturity_indices):
-            zcbond_prices[idx] = self.calc_price(spot, event_idx, maturity_idx)
+            zcbond_prices[idx] = self._calc_price(spot, event_idx, maturity_idx)
         return zcbond_prices
 
-    def calc_price(self,
-                   spot:  (float, np.ndarray),
-                   event_idx: int,
-                   maturity_idx: int) ->  (float, np.ndarray):
-        """Calculate price of zero coupon bond based on event_idx
-        (time t) and maturity_idx (time T).
-        - spot is the pseudo rate, i.e., x(t)
-        - Assuming event_grid[event_idx] < event_grid[maturity_idx]
-        Proposition 10.1.7, L.B.G. Andersen & V.V. Piterbarg 2010.
+###############################################################################
+
+    def _calc_price(self,
+                    spot: (float, np.ndarray),
+                    event_idx: int,
+                    maturity_idx: int) -> (float, np.ndarray):
+        """Calculate zero-coupon bond price.
+
+        Calculate price of zero-coupon bond based at current time
+        (event_idx) for maturity at time T (maturity_idx). See
+        proposition 10.1.7, L.B.G. Andersen & V.V. Piterbarg 2010.
+
+        Assuming event_grid[event_idx] < event_grid[maturity_idx].
+
+        Args:
+            spot: Spot value of the pseudo short rate.
+            event_idx: Event-grid index corresponding to current time.
+            maturity_idx: Event-grid index corresponding to maturity.
+
+        Returns:
+            Zero-coupon bond price.
         """
-        # P(0,t)
+        # P(0,t): Zero-coupon bond price at time zero with maturity t.
         price1 = self.discount_curve.values[event_idx]
-        # P(0,T)
+        # P(0,T): Zero-coupon bond price at time zero with maturity T.
         price2 = self.discount_curve.values[maturity_idx]
-        # Integration indices of the two relevant event dates
+        # Integration indices of the two relevant events.
         int_idx1 = self.int_event_idx[event_idx]
         int_idx2 = self.int_event_idx[maturity_idx] + 1
-        # Slice of integration grid
+        # Slice of integration grid.
         int_grid = self.int_grid[int_idx1:int_idx2]
-        # Slice of time-integrated kappa for each integration step
+        # Slice of time-integrated kappa for each integration step.
         int_kappa = self.int_kappa_step[int_idx1:int_idx2]
-        # G-function
-        # Eq. (10.18), L.B.G. Andersen & V.V. Piterbarg 2010
-        int_kappa = np.cumsum(int_kappa)
-        integrand = np.exp(-int_kappa)
+        # G(t,T): G-function,
+        # see Eq. (10.18), L.B.G. Andersen & V.V. Piterbarg 2010.
+        integrand = np.exp(-np.cumsum(int_kappa))
         g = np.sum(misc.trapz(int_grid, integrand))
-        # y-function
+        # y(t): y-function,
+        # see Eq. (10.17), L.B.G. Andersen & V.V. Piterbarg 2010.
         y = self.y_event_grid[event_idx]
         return price2 * np.exp(-spot * g - y * g ** 2 / 2) / price1
