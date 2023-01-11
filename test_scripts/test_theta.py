43a760f5574193ec7d6cebe067a7bdf53ef30b1c
Jakob Petersen <petersen.jakob@icloud.com>
2023-01-11 23:44:29 +0100

    Test theta method for put option in Black-Scholes model.

--- test_scripts/test_theta.py
+++ test_scripts/test_theta.py
@@ -2,14 +2,12 @@ from datetime import datetime
 import numpy as np
 
 from models.black_scholes import call as bs_call
+from models.black_scholes import put as bs_put
 from models.bachelier import call as ba_call
+from models.bachelier import put as ba_put
 from numerical_methods.finite_difference import theta
 from utils import payoffs
 from utils import plots
-
-n_doubles = 1
-
-# Test convergence wrt to time and space separately
 
 smoothing = False
 rannacher_stepping = False
@@ -19,35 +17,31 @@ show_plots = True
 model = "Black-Scholes"
 # model = "Bachelier"
 
-instrument = 'Call'
-# instrument = 'Put'
+# instrument = 'Call'
+instrument = 'Put'
 
 bc_type = "Linearity"
 # bc_type = "PDE"
 
-# solver_type = "AndersenPiterbarg"
-solver_type = "Andreasen"
+solver_type = "AndersenPiterbarg"
+# solver_type = "Andreasen"
 
 # Time execution
 start_time = datetime.now()
 
-rate = 0.1
+rate = 0.05
 strike = 50
 vol = 0.3
 expiry = 4
 
 t_min = 0
 t_max = expiry
-t_steps = 101
+t_steps = 21
 dt = (t_max - t_min) / (t_steps - 1)
 
 x_min = 5
-x_max = 145
+x_max = 125
 x_steps = 51
-
-t_array = np.zeros(n_doubles - 1)
-x_array = np.zeros(n_doubles - 1)
-norm_array = np.zeros((3, n_doubles - 1))
 
 # Reset current time
 t_current = t_max
@@ -90,6 +84,11 @@ if instrument == 'Call':
         instru = bs_call.Call(rate, vol, np.array([0, expiry]), strike, 1)
     elif model == "Bachelier":
         instru = ba_call.Call(vol, strike, expiry)
+elif instrument == 'Put':
+    if model == 'Black-Scholes':
+        instru = bs_put.Put(rate, vol, np.array([0, expiry]), strike, 1)
+    elif model == "Bachelier":
+        instru = ba_put.Put(vol, strike, expiry)
 
 plots.plot1(solver, payoff, solver.solution, instrument=instru, show=show_plots)
 
