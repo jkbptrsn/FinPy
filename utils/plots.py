f349014aba453cb260739b066fbfbad367a5bf1b
Jakob Petersen <petersen.jakob@icloud.com>
2023-01-18 21:09:49 +0100

    Added legends to plot1.

--- utils/plots.py
+++ utils/plots.py
@@ -12,42 +12,53 @@ def plot1(solver, payoff, price, instrument=None, show=True):
     f1, ax1 = plt.subplots(4, 1, sharex=True)
     f1.suptitle("Price and greeks of instrument")
 
-    # Price
-    ax1[0].plot(solver.grid(), payoff, 'k')
-    ax1[0].plot(solver.grid(), price, 'r')
+    # Plot of instrument payoff and price.
+    ax1[0].plot(solver.grid(), payoff, '-.k', label="Payoff")
+    ax1[0].plot(solver.grid(), price, '-r', label="Numerical result")
     if instrument:
         try:
-            ax1[0].plot(solver.grid(), instrument.price(solver.grid(), 0), 'ob', markersize=3)
+            ax1[0].plot(solver.grid(),
+                        instrument.price(solver.grid(), 0),
+                        'ob', markersize=2, label="Analytical result")
         except AttributeError:
             pass
     ax1[0].set_ylabel("Price")
     ax1[0].grid(True)
 
-    # Delta
-    ax1[1].plot(solver.grid(), solver.delta_fd(), 'r')
+    ax1[0].legend(bbox_to_anchor=(0, 1.02, 1, 0.2), loc="lower left",
+                  mode="expand", borderaxespad=0, ncol=3)
+
+    # Plot of instrument delta.
+    ax1[1].plot(solver.grid(), solver.delta_fd(), '-r')
     if instrument:
         try:
-            ax1[1].plot(solver.grid(), instrument.delta(solver.grid(), 0), 'ob', markersize=3)
+            ax1[1].plot(solver.grid(),
+                        instrument.delta(solver.grid(), 0),
+                        'ob', markersize=2)
         except (AttributeError, ValueError):
             print("Error in plots.py")
     ax1[1].set_ylabel("Delta")
     ax1[1].grid(True)
 
-    # Gamma
-    ax1[2].plot(solver.grid(), solver.gamma_fd(), 'r')
+    # Plot of instrument gamma.
+    ax1[2].plot(solver.grid(), solver.gamma_fd(), '-r')
     if instrument:
         try:
-            ax1[2].plot(solver.grid(), instrument.gamma(solver.grid(), 0), 'ob', markersize=3)
+            ax1[2].plot(solver.grid(),
+                        instrument.gamma(solver.grid(), 0),
+                        'ob', markersize=2)
         except (AttributeError, ValueError):
             print("Error in plots.py")
     ax1[2].set_ylabel("Gamma")
     ax1[2].grid(True)
 
-    # Theta
-    ax1[3].plot(solver.grid(), solver.theta_fd(), 'r')
+    # Plot of instrument theta.
+    ax1[3].plot(solver.grid(), solver.theta_fd(), '-r')
     if instrument:
         try:
-            ax1[3].plot(solver.grid(), instrument.theta(solver.grid(), 0), 'ob', markersize=3)
+            ax1[3].plot(solver.grid(),
+                        instrument.theta(solver.grid(), 0),
+                        'ob', markersize=2)
         except (AttributeError, ValueError):
             print("Error in plots.py")
     ax1[3].set_ylabel("Theta")
