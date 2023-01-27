cff97069dbf25625a2a1d9556107f7da0df5b0c7
Jakob Petersen <petersen.jakob@icloud.com>
2023-01-27 10:05:04 +0100

    Added legends to plot in test_put script.

--- tests/test_black_scholes/test_put.py
+++ tests/test_black_scholes/test_put.py
@@ -47,13 +47,17 @@ if __name__ == '__main__':
     b1 = binary.BinaryAssetPut(rate, vol, event_grid, strike, expiry_idx)
     b2 = binary.BinaryCashPut(rate, vol, event_grid, strike, expiry_idx)
 
-    plt.plot(spot, p.payoff(spot), '-k')
-    plt.plot(spot, p.price(spot, time_idx), '-ob')
-    plt.plot(spot, b1.price(spot, time_idx), '-r')
-    plt.plot(spot, strike * b2.price(spot, time_idx), '-g')
+    plt.plot(spot, p.payoff(spot), "-k", label="Option payoff")
+    plt.plot(spot, p.price(spot, time_idx), "-ob", label="Put")
+    plt.plot(spot, b1.price(spot, time_idx), "-r", label="Binary asset put")
+    plt.plot(spot, strike * b2.price(spot, time_idx), "-g", label="Binary cash put")
     put_price_decomposed = \
         - b1.price(spot, time_idx) + strike * b2.price(spot, time_idx)
-    plt.plot(spot, put_price_decomposed, '-y')
+    plt.plot(spot, put_price_decomposed, "-y", label="Combine binaries")
+    plt.title("Put option, Black-Scholes model")
+    plt.xlabel("Stock price")
+    plt.ylabel("Put price")
+    plt.legend()
     plt.show()
 
     unittest.main()
