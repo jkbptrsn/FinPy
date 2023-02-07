451da796adbd8c56e15400c9b26cd423ebd451a2
Jakob Petersen <petersen.jakob@icloud.com>
2023-02-07 21:13:49 +0100

    Docstrings added.

--- utils/payoffs.py
+++ utils/payoffs.py
@@ -3,40 +3,95 @@ import numpy as np
 
 def binary_asset_call(spot: (float, np.ndarray),
                       strike: float) -> (float, np.ndarray):
-    """Long asset-or-nothing call option position."""
+    """Long asset-or-nothing call option position.
+
+    Args:
+        spot: Spot price.
+        strike: Strike price.
+
+    Returns:
+        Payoff.
+    """
     return (spot > strike) * spot
 
 
 def binary_asset_put(spot: (float, np.ndarray),
                      strike: float) -> (float, np.ndarray):
-    """Long asset-or-nothing put option position."""
+    """Long asset-or-nothing put option position.
+
+    Args:
+        spot: Spot price.
+        strike: Strike price.
+
+    Returns:
+        Payoff.
+    """
     return (spot < strike) * spot
 
 
 def binary_cash_call(spot: (float, np.ndarray),
                      strike: float) -> (float, np.ndarray):
-    """Long cash-or-nothing call option position."""
+    """Long cash-or-nothing call option position.
+
+    Args:
+        spot: Spot price.
+        strike: Strike price.
+
+    Returns:
+        Payoff.
+    """
     return (spot > strike) * 1
 
 
 def binary_cash_put(spot: (float, np.ndarray),
                     strike: float) -> (float, np.ndarray):
-    """Long cash-or-nothing put option position."""
+    """Long cash-or-nothing put option position.
+
+    Args:
+        spot: Spot price.
+        strike: Strike price.
+
+    Returns:
+        Payoff.
+    """
     return (spot < strike) * 1
 
 
 def call(spot: (float, np.ndarray),
          strike: float) -> (float, np.ndarray):
-    """Long call option position."""
+    """Long call option position.
+
+    Args:
+        spot: Spot price.
+        strike: Strike price.
+
+    Returns:
+        Payoff.
+    """
     return np.maximum(spot - strike, 0)
 
 
 def put(spot: (float, np.ndarray),
         strike: float) -> (float, np.ndarray):
-    """Long put option position."""
+    """Long put option position.
+
+    Args:
+        spot: Spot price.
+        strike: Strike price.
+
+    Returns:
+        Payoff.
+    """
     return np.maximum(strike - spot, 0)
 
 
 def zero_coupon_bond(spot: (float, np.ndarray)) -> (float, np.ndarray):
-    """Long zero-coupon bond position."""
+    """Long zero-coupon bond position.
+
+    Args:
+        spot: Spot rate.
+
+    Returns:
+        Payoff.
+    """
     return 0 * spot + 1
