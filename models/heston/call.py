2a879cf59d5ba9f299d3311d711ec8cbeb4f7856
Jakob Petersen <petersen.jakob@icloud.com>
2023-03-19 13:47:44 +0100

    Pricing of call option in Heston model, comparison of FD solution and analytical solution.

--- models/heston/call.py
+++ models/heston/call.py
@@ -1,6 +1,9 @@
+import cmath
+import math
 import typing
 
 import numpy as np
+import scipy
 
 from models import options
 from utils import global_types
@@ -56,18 +59,74 @@ class Call(options.EuropeanOption2D):
         return payoffs.call(spot, self.strike)
 
     def price(self,
-              spot: typing.Union[float, np.ndarray],
+              spot_price: float,
+              spot_variance: float,
               event_idx: int) -> typing.Union[float, np.ndarray]:
         """Price function.
 
         Args:
-            spot: Current stock price.
+            spot_price: Current stock price.
+            spot_variance: Current variance of stock price.
             event_idx: Index on event grid.
 
         Returns:
             Price.
         """
-        pass
+        # Time to maturity.
+        tau = self.expiry - self.event_grid[event_idx]
+        integral = self._integral(spot_price, spot_variance, tau)
+        return spot_price \
+            - self.strike * math.exp(-self.rate * tau) * integral / math.pi
+
+    def _phi(self,
+             u: float,
+             spot_variance: float,
+             tau: float):
+        """Characteristic function for Heston model.
+
+        See A. Lipton, Risk, 2002.
+
+        Args:
+            u: Independent variable.
+            spot_variance: Current variance of stock price.
+            tau: Time to maturity.
+
+        Returns:
+            Characteristic function.
+        """
+        alpha = -u * (u + 1j) / 2
+        beta = self.kappa - 1j * u * self.vol * self.correlation
+        gamma = self.vol ** 2 / 2
+        d = cmath.sqrt(beta ** 2 - 4 * alpha * gamma)
+        g = (beta - d) / (beta + d)
+        h = cmath.exp(-d * tau)
+        a = (beta - d) * tau - 2 * cmath.log((g * h - 1) / (g - 1))
+        a = self.kappa * self.eta * a / self.vol ** 2
+        b = (beta - d) * (1 - h) / (self.vol ** 2 * (1 - g * h))
+        return cmath.exp(a + b * spot_variance)
+
+    def _integral(self,
+                  spot_price: float,
+                  spot_variance: float,
+                  tau: float):
+        """Integral in call option pricing function.
+
+        Args:
+            spot_price: Current stock price.
+            spot_variance: Current variance of stock price.
+            tau: Time to maturity.
+
+        Returns:
+            Integral...
+        """
+        k = math.log(spot_price / self.strike) + self.rate * tau
+        integrand = \
+            (lambda u:
+             np.real(np.exp((1j * u + 0.5) * k)
+                     * self._phi(u - 0.5j, spot_variance, tau))
+             / (u ** 2 + 0.25))
+        integral, err = scipy.integrate.quad_vec(integrand, 0, np.inf)
+        return integral
 
     def fd_solve(self):
         """Run solver on event_grid..."""
--- unit_tests/test_heston/test_call.py
+++ unit_tests/test_heston/test_call.py
@@ -1,14 +1,10 @@
-import math
 import unittest
 
 from matplotlib import pyplot as plt
 from matplotlib import cm
 import numpy as np
-from scipy.stats import linregress
 
 from models.heston import call
-from numerics.fd.adi import craig_sneyd as cs
-from numerics.fd import misc
 
 plot_result = True
 print_result = False
@@ -19,15 +15,15 @@ class CallOption(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        self.x_min = 0
+        self.x_min = 2
         self.x_max = 400
-        self.x_steps = 401
+        self.x_steps = 101
         self.dx = (self.x_max - self.x_min) / (self.x_steps - 1)
         self.x_grid = self.dx * np.arange(self.x_steps) + self.x_min
 
         self.y_min = 0
         self.y_max = 1
-        self.y_steps = 101
+        self.y_steps = 21
         self.dy = (self.y_max - self.y_min) / (self.y_steps - 1)
         self.y_grid = self.dy * np.arange(self.y_steps) + self.y_min
 
@@ -39,11 +35,11 @@ class CallOption(unittest.TestCase):
         self.eta = 0.12
         self.vol = 0.041
         self.correlation = 0.6
-        self.strike = 200
+        self.strike = 100
 
         self.t_min = 0
-        self.t_max = 0.2
-        self.t_steps = 401
+        self.t_max = 1
+        self.t_steps = 31
         self.dt = (self.t_max - self.t_min) / (self.t_steps - 1)
         self.event_grid = self.dt * np.arange(self.t_steps) + self.t_min
         self.expiry_idx = self.event_grid.size - 1
@@ -55,12 +51,18 @@ class CallOption(unittest.TestCase):
 
     def test_1(self):
         """..."""
-
+        # Numerical result.
         self.instrument.fd_solve()
+        # Analytical result.
+        a_result = np.zeros(self.instrument.fd.solution.shape)
+        for idx_x, x in enumerate(self.instrument.fd.grid_x):
+            for idx_y, y in enumerate(self.instrument.fd.grid_y):
+                a_result[idx_x, idx_y] = self.instrument.price(x, y, 0)
 
         if plot_result:
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
+            fig = plt.figure(figsize=plt.figaspect(0.5))
+
+            ax = fig.add_subplot(1, 2, 1, projection='3d')
             plot_x, plot_y = np.meshgrid(self.y_grid, self.x_grid)
             ax.plot_surface(plot_x, plot_y, self.instrument.fd.solution, cmap=cm.jet)
             ax.set_xlabel("Variance")
@@ -68,9 +70,17 @@ class CallOption(unittest.TestCase):
             ax.set_zlabel("Option Price")
             ax.set_xlim([self.y_min, self.y_max])
             ax.set_ylim([self.x_min, self.x_max])
-            plt.show()
 
-        print(self.instrument.fd.solution[(self.x_steps - 1) // 2, (self.y_steps - 1) // 2])
+            ax = fig.add_subplot(1, 2, 2, projection='3d')
+            plot_x, plot_y = np.meshgrid(self.y_grid, self.x_grid)
+            diff = np.abs(self.instrument.fd.solution - a_result)
+            ax.plot_surface(plot_x, plot_y, diff, cmap=cm.jet)
+            ax.set_xlabel("Variance")
+            ax.set_ylabel("Stock Price")
+            ax.set_zlabel("Price difference")
+            ax.set_xlim([self.y_min, self.y_max])
+            ax.set_ylim([self.x_min, self.x_max])
+            plt.show()
 
 
 if __name__ == '__main__':
