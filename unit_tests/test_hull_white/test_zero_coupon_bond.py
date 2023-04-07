Calculation of g-function in 1-factor Hull-White model is revised. Bug found in pricing of zero-coupon bond using theta method.

unit_tests/test_hull_white/test_zero_coupon_bond.py
@@ -90,10 +90,10 @@ class ZeroCouponBond(unittest.TestCase):
         self.discount_curve = input.disc_curve
 
         # Bond maturity.
-        self.maturity = 2.5
+        self.maturity = 10
 
         # FD event grid.
-        self.fd_t_steps = 201
+        self.fd_t_steps = 101
         self.fd_dt = self.maturity / (self.fd_t_steps - 1)
         self.fd_event_grid = self.fd_dt * np.arange(self.fd_t_steps)
         self.fd_maturity_idx = self.fd_t_steps - 1
@@ -104,7 +104,7 @@ class ZeroCouponBond(unittest.TestCase):
         # FD spatial grid.
         self.x_min = -0.1
         self.x_max = 0.25
-        self.x_steps = 200
+        self.x_steps = 100
         self.dx = (self.x_max - self.x_min) / (self.x_steps - 1)
         self.x_grid = self.dx * np.arange(self.x_steps) + self.x_min
 
@@ -130,22 +130,20 @@ class ZeroCouponBond(unittest.TestCase):
         self.bond.fd_setup(self.x_grid, equidistant=True)
         self.bond.fd_solve()
         numerical = self.bond.fd.solution
+        numerical *= self.bond.discount_curve_eg[self.bond.maturity_idx]
         analytical = self.bond.price(self.x_grid, 0)
         relative_error = np.abs((analytical - numerical) / analytical)
+        print("max error: ", np.max(relative_error[10:40]))
         if plot_results:
 #            plt.plot(self.x_grid, relative_error, "-b")
 #            plt.plot(self.x_grid - 0.027, numerical, "-r")
             plt.plot(self.x_grid, numerical, "-r")
-            plt.plot(self.x_grid, analytical, "-k")
+            plt.plot(self.x_grid, analytical, "-ok")
 #            plt.plot(self.x_grid, self.bond.delta(self.x_grid, 0), "-r")
 #            plt.plot(self.x_grid, self.bond.gamma(self.x_grid, 0), "-k")
             plt.xlabel("Short rate")
             plt.ylabel("Price/Delta/Gamma")
             plt.pause(5)
-
-        print(self.bond.price(-0.003, 0))
-        print(self.bond.delta(-0.003, 0))
-        print(self.bond.gamma(-0.003, 0))
 
 
 if __name__ == '__main__':