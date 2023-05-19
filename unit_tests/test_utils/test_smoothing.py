2bb16d7ed3a57597a3b8381e024e5ec1b126b305
Jakob Petersen <petersen.jakob@icloud.com>
2023-05-19 18:09:26 +0200

    Box smoothing of discrete payoff function.

---
+++ unit_tests/test_utils/test_smoothing.py
@@ -0,0 +1,42 @@
+import unittest
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+from utils import payoffs
+from utils import smoothing
+
+plot_results = False
+print_results = False
+
+
+class Smoothing(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.x_min = 32
+        self.x_max = 70
+        self.n_steps = 11
+        self.dx = (self.x_max - self.x_min) / (self.n_steps - 1)
+        self.x_grid = self.dx * np.arange(self.n_steps) + self.x_min
+
+    @staticmethod
+    def payoff(spot):
+        return payoffs.call(spot, 50)
+
+    def test_this(self):
+        if plot_results:
+            p = self.payoff(self.x_grid)
+            plt.plot(self.x_grid, p, "ob", label="Payoff")
+            p_smooth = smoothing.smoothing_payoff_1d(self.x_grid, self)
+            plt.plot(self.x_grid, p_smooth, "or", label="Smooth payoff")
+            p_linear = smoothing.smoothing_1d(self.x_grid, p)
+            plt.plot(self.x_grid, p_linear, "xk", label="Linear payoff")
+            plt.xlabel("Stock price")
+            plt.ylabel("Payoff")
+            plt.legend()
+            plt.show()
+        self.assertEqual(2, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- utils/smoothing.py
+++ utils/smoothing.py
@@ -24,9 +24,43 @@ def smoothing_1d(grid: np.ndarray,
     # Loop over interior points.
     for idx in range(grid.size - 2):
         average_step_size = (step_size[idx + 1] + step_size[idx]) / 2
-        weight_left = step_size[idx] / (2 * average_step_size)
-        weight_right = step_size[idx + 1] / (2 * average_step_size)
+        weight_left = step_size[idx + 1] / (2 * average_step_size)
+        weight_right = step_size[idx] / (2 * average_step_size)
         f_left = weight_left * f_midpoint[idx]
         f_right = weight_right * f_midpoint[idx + 1]
+        smooth[idx + 1] = f_left + f_right
+    return smooth
+
+
+def smoothing_payoff_1d(grid: np.ndarray,
+                        instrument) -> np.ndarray:
+    """Box smoothing of payoff function on non-equidistant grid.
+
+    See L.B.G. Andersen & V.V. Piterbarg 2010, section 23.3.
+
+    Args:
+        grid: Discrete grid.
+        instrument: Instrument object with 'payoff' method.
+
+    Returns:
+        Smooth payoff.
+    """
+    # Extended grid with midpoints.
+    extended_grid = np.zeros(2 * grid.size - 1)
+    extended_grid[::2] = grid
+    for idx in range(grid.size - 1):
+        extended_grid[2 * idx + 1] = (grid[idx] + grid[idx + 1]) / 2
+    payoff = instrument.payoff(extended_grid)
+    smooth = np.zeros(grid.size)
+    smooth[0] = payoff[0]
+    smooth[-1] = payoff[-1]
+    step_size = np.diff(grid)
+    # Loop over interior points.
+    for idx in range(grid.size - 2):
+        average_step_size = (step_size[idx + 1] + step_size[idx]) / 2
+        weight_left = step_size[idx + 1] / (2 * average_step_size)
+        weight_right = step_size[idx] / (2 * average_step_size)
+        f_left = weight_left * payoff[2 * idx + 1]
+        f_right = weight_right * payoff[2 * idx + 3]
         smooth[idx + 1] = f_left + f_right
     return smooth
