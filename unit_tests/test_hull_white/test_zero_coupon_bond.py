f3ab307221a53f88e6e94847b006515fbf247c92
Jakob Petersen <petersen.jakob@icloud.com>
2023-07-23 12:39:27 +0200

    Unit tests of SdeExact classes revisited.

--- unit_tests/test_hull_white/test_zero_coupon_bond.py
+++ unit_tests/test_hull_white/test_zero_coupon_bond.py
@@ -205,10 +205,7 @@ class ZeroCouponBond(unittest.TestCase):
     def setUp(self) -> None:
         # Model parameters.
         self.kappa = input.kappa_strip
-
-#        self.vol = input.vol_strip_constant
         self.vol = input.vol_strip
-
         self.discount_curve = input.disc_curve
         # Bond maturity.
         self.maturity = 30
@@ -223,132 +220,176 @@ class ZeroCouponBond(unittest.TestCase):
         self.x_steps = 301
         self.dx = (self.x_max - self.x_min) / (self.x_steps - 1)
         self.x_grid = self.dx * np.arange(self.x_steps) + self.x_min
-        self.int_step_factor = 50  # 200 # 3
-        self.int_step_size = self.fd_dt / self.int_step_factor
-        # Zero-coupon bond.
-
-#        self.time_dependence = "constant"
+        # Zero-coupon bonds.
         self.time_dependence = "piecewise"
-#        self.time_dependence = "general"
-
         self.bond = zcbond.ZCBond(self.kappa,
                                   self.vol,
                                   self.discount_curve,
                                   self.fd_maturity_idx,
                                   self.fd_event_grid,
-                                  self.time_dependence,
-                                  self.int_step_size)
-
+                                  self.time_dependence)
         self.bond_pelsser = \
             zcbond.ZCBondPelsser(self.kappa,
                                  self.vol,
                                  self.discount_curve,
                                  self.fd_maturity_idx,
                                  self.fd_event_grid,
-                                 self.time_dependence,
-                                 self.int_step_size)
+                                 self.time_dependence)
 
     def test_theta_method(self):
         """Finite difference pricing of zero-coupon bond."""
+        print(self.bond.transformation)
         self.bond.fd_setup(self.x_grid, equidistant=True)
         self.bond.fd_solve()
+        # Check price.
         numerical = self.bond.fd.solution
         analytical = self.bond.price(self.x_grid, 0)
         relative_error = np.abs((analytical - numerical) / analytical)
         if plot_results:
             plots.plot_price_and_greeks(self.bond)
-        # Maximum error in interval around pseudo short rate of 0.
+        # Maximum error in interval around pseudo short rate of 0.1.
         idx_min = np.argwhere(self.x_grid < -0.05)[-1][0]
         idx_max = np.argwhere(self.x_grid < 0.05)[-1][0]
         max_error = np.max(relative_error[idx_min:idx_max + 1])
         if print_results:
-            print("max error: ", max_error)
-        self.assertTrue(max_error < 2.e-3)
-        # TODO: Compare all greeks...
+            print(f"Maximum error of price: {max_error:2.5f}")
+        self.assertTrue(max_error < 6.3e-3)
+        # Check delta.
+        numerical = self.bond.fd.delta()
+        analytical = self.bond.delta(self.x_grid, 0)
+        relative_error = np.abs((analytical - numerical) / analytical)
+        max_error = np.max(relative_error[idx_min:idx_max + 1])
+        if print_results:
+            print(f"Maximum error of delta: {max_error:2.5f}")
+        self.assertTrue(max_error < 2.1e-3)
+        # Check gamma.
+        numerical = self.bond.fd.gamma()
+        analytical = self.bond.gamma(self.x_grid, 0)
+        relative_error = np.abs((analytical - numerical) / analytical)
+        max_error = np.max(relative_error[idx_min:idx_max + 1])
+        if print_results:
+            print(f"Maximum error of gamma: {max_error:2.5f}")
+        self.assertTrue(max_error < 3.7e-3)
+        # Check theta.
+        numerical = self.bond.fd.theta()
+        analytical = self.bond.theta(self.x_grid, 0)
+        error = np.abs((analytical - numerical))
+        max_error = np.max(error[idx_min:idx_max + 1])
+        if print_results:
+            print(f"Maximum error of theta: {max_error:2.5f}")
+        self.assertTrue(max_error < 6.8e-3)
 
     def test_theta_method_pelsser(self):
         """Finite difference pricing of zero-coupon bond."""
         print(self.bond_pelsser.transformation)
         self.bond_pelsser.fd_setup(self.x_grid, equidistant=True)
         self.bond_pelsser.fd_solve()
+        # Check price.
         numerical = self.bond_pelsser.fd.solution
         analytical = self.bond_pelsser.price(self.x_grid, 0)
         relative_error = np.abs((analytical - numerical) / analytical)
         if plot_results:
             plots.plot_price_and_greeks(self.bond_pelsser)
-        # Maximum error in interval around pseudo short rate of 0.
+        # Maximum error in interval around pseudo short rate of 0.1.
         idx_min = np.argwhere(self.x_grid < -0.05)[-1][0]
         idx_max = np.argwhere(self.x_grid < 0.05)[-1][0]
         max_error = np.max(relative_error[idx_min:idx_max + 1])
         if print_results:
-            print("max error: ", max_error)
-        self.assertTrue(max_error < 2.e-3)
-        # TODO: Compare all greeks...
+            print(f"Maximum error of price: {max_error:2.5f}")
+        self.assertTrue(max_error < 2.0e-3)
+        # Check delta.
+        numerical = self.bond_pelsser.fd.delta()
+        analytical = self.bond_pelsser.delta(self.x_grid, 0)
+        relative_error = np.abs((analytical - numerical) / analytical)
+        max_error = np.max(relative_error[idx_min:idx_max + 1])
+        if print_results:
+            print(f"Maximum error of delta: {max_error:2.5f}")
+        self.assertTrue(max_error < 2.7e-3)
+        # Check gamma.
+        numerical = self.bond_pelsser.fd.gamma()
+        analytical = self.bond_pelsser.gamma(self.x_grid, 0)
+        relative_error = np.abs((analytical - numerical) / analytical)
+        max_error = np.max(relative_error[idx_min:idx_max + 1])
+        if print_results:
+            print(f"Maximum error of gamma: {max_error:2.5f}")
+        self.assertTrue(max_error < 6.0e-3)
+        # Check theta.
+        numerical = self.bond_pelsser.fd.theta()
+        analytical = self.bond_pelsser.theta(self.x_grid, 0)
+        error = np.abs((analytical - numerical))
+        max_error = np.max(error[idx_min:idx_max + 1])
+        if print_results:
+            print(f"Maximum error of theta: {max_error:2.5f}")
+        self.assertTrue(max_error < 1.3e-3)
 
-    def test_monte_carlo(self):
+    def test_monte_carlo_exact(self):
         """Monte-Carlo pricing of zero-coupon bond."""
-        x_grid = 0.02 * np.arange(11) - 0.1
-        analytical = self.bond.price(x_grid, 0)
-        n_paths = 30000
-        # TODO: Why choose time-dependence?
-#        self.bond.mc_exact_setup(time_dependence="piecewise")
         self.bond.mc_exact_setup()
-        numerical = np.zeros(x_grid.size)
-        error = np.zeros(x_grid.size)
+        # Spot rate.
+        spot = 0.02
+        spot_vector = spot * np.arange(11) - 0.1
+        # Initialize random number generator.
         rng = np.random.default_rng(0)
-        for idx, spot in enumerate(x_grid):
-            self.bond.mc_exact_solve(spot, n_paths, rng=rng, antithetic=True)
+        # Number of paths for each Monte-Carlo estimate.
+        n_paths = 2000
+        # Analytical result.
+        price_a = self.bond.price(spot_vector, 0)
+        numerical = np.zeros(spot_vector.size)
+        error = np.zeros(spot_vector.size)
+        for idx, s in enumerate(spot_vector):
+            self.bond.mc_exact_solve(s, n_paths, rng=rng, antithetic=True)
             numerical[idx] = self.bond.mc_exact.mc_estimate
             error[idx] = self.bond.mc_exact.mc_error
         if plot_results:
-            plt.plot(x_grid, analytical, "-b")
-            plt.errorbar(x_grid, numerical, yerr=error, fmt="or")
+            plt.plot(spot_vector, price_a, "-b")
+            plt.errorbar(spot_vector, numerical, yerr=error,
+                         fmt='or', markersize=2, capsize=5)
             plt.xlabel("Initial pseudo short rate")
             plt.ylabel("Zero-coupon bond price")
             plt.show()
-        relative_error = np.abs((analytical - numerical) / analytical)
+        relative_error = np.abs((price_a - numerical) / price_a)
         # Maximum error in interval around pseudo short rate of 0.
-        idx_min = np.argwhere(x_grid < -0.05)[-1][0]
-        idx_max = np.argwhere(x_grid < 0.05)[-1][0]
+        idx_min = np.argwhere(spot_vector < -0.05)[-1][0]
+        idx_max = np.argwhere(spot_vector < 0.05)[-1][0]
         max_error = np.max(relative_error[idx_min:idx_max + 1])
         if print_results:
             print("max error: ", max_error)
-        self.assertTrue(max_error < 5e-3)
+        self.assertTrue(max_error < 1.1e-2)
 
-    def test_monte_carlo_pelsser(self):
+    def test_monte_carlo_pelsser_exact(self):
         """Monte-Carlo pricing of zero-coupon bond."""
-        x_grid = 0.02 * np.arange(11) - 0.1
-        analytical = self.bond_pelsser.price(x_grid, 0)
-        n_paths = 30000
-        # TODO: Why choose time-dependence?
-#        self.bond_pelsser.mc_exact_setup(time_dependence="piecewise")
         self.bond_pelsser.mc_exact_setup()
-        numerical = np.zeros(x_grid.size)
-        error = np.zeros(x_grid.size)
+        # Spot rate.
+        spot = 0.02
+        spot_vector = spot * np.arange(11) - 0.1
+        # Initialize random number generator.
         rng = np.random.default_rng(0)
-        for idx, spot in enumerate(x_grid):
-#            self.bond_pelsser.mc_exact_solve(spot, n_paths, rng=rng, antithetic=True)
-#            numerical[idx] = self.bond_pelsser.mc_exact.solution
-#            error[idx] = self.bond_pelsser.mc_exact.error
-
-            self.bond_pelsser.mc_exact_solve(spot, n_paths, rng=rng, antithetic=True)
+        # Number of paths for each Monte-Carlo estimate.
+        n_paths = 2000
+        # Analytical result.
+        price_a = self.bond_pelsser.price(spot_vector, 0)
+        numerical = np.zeros(spot_vector.size)
+        error = np.zeros(spot_vector.size)
+        for idx, s in enumerate(spot_vector):
+            self.bond_pelsser.mc_exact_solve(s, n_paths, rng=rng,
+                                             antithetic=True)
             numerical[idx] = self.bond_pelsser.mc_exact.mc_estimate
             error[idx] = self.bond_pelsser.mc_exact.mc_error
-
         if plot_results:
-            plt.plot(x_grid, analytical, "-b")
-            plt.errorbar(x_grid, numerical, yerr=error, fmt="or")
+            plt.plot(spot_vector, price_a, "-b")
+            plt.errorbar(spot_vector, numerical, yerr=error,
+                         fmt='or', markersize=2, capsize=5)
             plt.xlabel("Initial pseudo short rate")
             plt.ylabel("Zero-coupon bond price")
             plt.show()
-        relative_error = np.abs((analytical - numerical) / analytical)
+        relative_error = np.abs((price_a - numerical) / price_a)
         # Maximum error in interval around pseudo short rate of 0.
-        idx_min = np.argwhere(x_grid < -0.05)[-1][0]
-        idx_max = np.argwhere(x_grid < 0.05)[-1][0]
+        idx_min = np.argwhere(spot_vector < -0.05)[-1][0]
+        idx_max = np.argwhere(spot_vector < 0.05)[-1][0]
         max_error = np.max(relative_error[idx_min:idx_max + 1])
         if print_results:
             print("max error: ", max_error)
-        self.assertTrue(max_error < 5e-3)
+        self.assertTrue(max_error < 1.1e-2)
 
 
 if __name__ == '__main__':
