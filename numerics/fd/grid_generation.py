e601e709913b3b281f7f766e87f3b01ba5cf3624
jkbptrsn <petersen.jakob@icloud.com>
2024-04-10 22:08:15 +0200

    grid_generation.py revisited.

--- numerics/fd/grid_generation.py
+++ numerics/fd/grid_generation.py
@@ -4,35 +4,34 @@ import numpy as np
 
 def equidistant(x_min: float,
                 x_max: float,
-                x_states: int):
-    """Equidistant grid in 1-dimension.
+                n_states: int):
+    """Equidistant grid in one dimension.
 
     Args:
-        x_min: Left boundary of domain.
-        x_max: Right boundary of domain.
-        x_states: Number of grid points.
+        x_min: "Left" boundary of domain.
+        x_max: "Right" boundary of domain.
+        n_states: Number of grid points.
 
     Returns:
         Equidistant grid.
     """
-    dx = (x_max - x_min) / (x_states - 1)
-    return dx * np.arange(x_states) + x_min
+    dx = (x_max - x_min) / (n_states - 1)
+    return dx * np.arange(n_states) + x_min
 
 
 def hyperbolic(x_min: float,
                x_max: float,
-               x_states: int,
+               n_states: int,
                x_center: float,
                shape: float = None):
-    """Non-equidistant grid in 1-dimension.
+    """Non-equidistant grid in one dimension.
 
-    Hyperbolic sine transformation. See,
-    K.J. In 'T Hout and S. Foulon, 2010.
+    Hyperbolic sine transformation. See In 'T Hout and Foulon (2010).
 
     Args:
-        x_min: Left boundary of domain.
-        x_max: Right boundary of domain.
-        x_states: Number of grid points.
+        x_min: "Left" boundary of domain.
+        x_max: "Right" boundary of domain.
+        n_states: Number of grid points.
         x_center: "Center" of non-equidistant grid.
         shape: Controls the fraction of grid points in the neighbourhood
             of x_center. Default is x_center / 5.
@@ -41,10 +40,10 @@ def hyperbolic(x_min: float,
         Non-equidistant grid.
     """
     if x_center < x_min or x_center > x_max:
-        raise ValueError("Grid 'center' is no contained in domain.")
+        raise ValueError("Grid 'center' is not contained in domain.")
     if not shape:
         shape = x_center / 5
-    lower = math.asinh(- (x_center - x_min) / shape)
-    delta = (math.asinh((x_max - x_center) / shape) - lower) / (x_states - 1)
-    grid = delta * np.arange(x_states) + lower
+    lower = math.asinh(-(x_center - x_min) / shape)
+    delta = (math.asinh((x_max - x_center) / shape) - lower) / (n_states - 1)
+    grid = delta * np.arange(n_states) + lower
     return grid, x_center + shape * np.sinh(grid)
