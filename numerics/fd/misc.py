12ec48b35e129cf8e4dfbfba5a4cf7ccda75d6f5
jkbptrsn <petersen.jakob@icloud.com>
2024-04-12 18:34:59 +0200

    misc.py revisited.

--- numerics/fd/misc.py
+++ numerics/fd/misc.py
@@ -11,7 +11,7 @@ def norms_1d(vector1: np.ndarray,
         vector1: Solution on grid1.
         vector2: Solution on grid2.
         step_size1: Step size of grid1.
-        slice_nr: Ratio of number of steps of grid1 and grid2.
+        slice_nr: Ratio of number of grid points of grid1 and grid2.
 
     Returns:
         Center norm, max norm, L2 norm.
@@ -32,27 +32,27 @@ def norms_1d(vector1: np.ndarray,
 def norms_2d(vector1: np.ndarray,
              vector2: np.ndarray,
              step_size1: (float, float),
-             direction: str = "x",
+             dimension: str = "x",
              slice_nr: int = 2) -> (float, float, float):
     """Calculate norms of 2-dimensional vector.
 
     Args:
         vector1: Solution on grid1.
         vector2: Solution on grid2.
-        step_size1: Step size of grid1 (in x- and y-dimension).
-        direction: Dimension in which the step size has changed...
-        slice_nr: Ratio of number of steps of grid1 and grid2.
+        step_size1: Step sizes of grid1 (in x- and y-dimension).
+        dimension: Dimension in which the step size has changed.
+        slice_nr: Ratio of number of grid points of grid1 and grid2.
 
     Returns:
         Center norm, max norm, L2 norm.
     """
     # Absolute difference.
-    if direction == "x":
+    if dimension == "x":
         diff = np.abs(vector1 - vector2[::slice_nr, :])
-    elif direction == "y":
+    elif dimension == "y":
         diff = np.abs(vector1 - vector2[:, ::slice_nr])
     else:
-        raise ValueError("Direction is unknown. Should be x or y.")
+        raise ValueError("Unknown dimension. Should be x or y.")
     # "Center" norm.
     n_states = diff.shape
     idx_center = (n_states[0] - 1) // 2, (n_states[1] - 1) // 2
